89ae4946a31de50f904ef6f1ba2978fc4a87d87d
ay.develop <ay.develop@gmail.com>
2019-11-13 17:57:39 +0300

    4523

---
+++ src/components/Tasks/Task.vue
@@ -0,0 +1,32 @@
+<template>
+    <q-item
+        :class="task.complete ? 'bg-green-1': 'bg-orange-1'"
+        tag="label" v-ripple>
+            <q-item-section side top>
+                <q-checkbox v-model="task.complete" />
+            </q-item-section>
+
+            <q-item-section>
+                <q-item-label :class="{'text-strike' : task.complete}">
+                    {{ task.name }} / {{ id }}
+                </q-item-label>
+            </q-item-section>
+
+            <q-item-section side top>
+                <q-icon name="event" />               
+                <q-item-label caption>{{ task.dueDate }}</q-item-label>
+                <q-item-label caption>{{ task.dueTime }}</q-item-label>
+            </q-item-section>
+        
+        </q-item>
+</template>
+
+<script>
+    export default {
+        props: ['task','id'],
+        data:()=>({ })
+    }
+</script>
+
+<style scoped>
+</style>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -2,28 +2,12 @@
     <q-page class="q-pa-md">
         <q-list bordered  separator>
       
-        <q-item
-        :class="task.complete ? 'bg-green-1': 'bg-orange-1'"
-        v-for="(task, key ) in tasks"
-        :key="task.id"
-        tag="label" v-ripple>
-            <q-item-section side top>
-                <q-checkbox v-model="task.complete" />
-            </q-item-section>
-
-            <q-item-section>
-                <q-item-label :class="{'text-strike' : task.complete}">
-                    {{ task.name }} / {{ key }}
-                </q-item-label>
-            </q-item-section>
-
-            <q-item-section side top>
-                <q-icon name="event" />               
-                <q-item-label caption>{{ task.dueDate }}</q-item-label>
-                <q-item-label caption>{{ task.dueTime }}</q-item-label>
-            </q-item-section>
-        
-        </q-item>
+            <task v-for="(task, key) 
+                in tasks"
+                :key="key"
+                :task="task"
+                :id="key"
+            ></task>
 
         </q-list>
     </q-page>
@@ -33,6 +17,9 @@
     import { mapGetters } from 'vuex'
 
     export default {
+        components:{
+            'task' : require('components/Tasks/Task.vue').default
+        },
         computed:{
             ...mapGetters('tasks', ['tasks'])
         }

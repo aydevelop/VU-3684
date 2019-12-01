5c242e0fe63877a785f86a280fd924e8d14b358e
ay.develop <ay.develop@gmail.com>
2019-12-01 18:29:40 +0300

    Auth check fix

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,11 +1,12 @@
 
 import { LocalStorage, SessionStorage } from 'quasar'
 // "async" is optional
-export default async ({ router }) => {
+export default ({ router }) => {
    router.beforeEach((to, from, next)=>{
       let loggedIn2 = localStorage.getItem('loggedIn')
+      console.log("loggedIn2: "+loggedIn2)
 
-      if(loggedIn2==0){
+      if(loggedIn2==null || loggedIn2==0){
          if(to.path != '/auth'){
             if(router.currentRoute.path != '/auth'){
                next('/auth')               
